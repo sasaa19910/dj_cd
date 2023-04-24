4427b70f639563628829244bb7bf5e537391cad2
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-04-24 18:20:11 +0300

    Add ÐFile

--- logistic/views.py
+++ logistic/views.py
@@ -25,4 +25,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "Всем большой привет!"})
+        return Response({"message": "Допустим!"})
